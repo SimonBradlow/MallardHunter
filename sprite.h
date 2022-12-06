52f43d4ce1b3499c15602b69c1ec748c24c5688e
Simon Bradlow <simon@bradlow.com>
2022-12-06 18:22:11 -0500

    sprite class converted to 2d vector of optionals

--- sprite.h
+++ sprite.h
@@ -6,6 +6,11 @@
 #include "rect.h"
 #include <fstream>
 #include <vector>
+#include <memory>
+#include <optional>
+using std::optional;
+using std::nullopt;
+using std::make_optional;
 
 #ifdef _WIN32
 // If your Windows machine runs Python in CLI with "python" instead of "py", update this line.
@@ -19,38 +24,43 @@ class Sprite : public Rect {
 private:
     double scale;
 public:
-    std::vector<std::vector<color>> vec;
+    std::vector<std::vector<optional<color>>> vec;
+
     Sprite() {
         scale = 1;
     }
+
     Sprite(double s) {
         if (s > 0) scale = s;
         else scale = 1;
     }
+
     void setScale(double s){
         if (s > 0) scale = s;
     }
-    void setVec(std::vector<std::vector<color>> &v) {
+
+    void setVec(std::vector<std::vector<optional<color>>> &v) {
         vec = v;
         setWidth(v[0].size()*scale);
         setHeight(v.size()*scale);
     }
+
     void setVec(Sprite &o) {
-        std::vector<std::vector<color>> v = o.vec;
+        std::vector<std::vector<optional<color>>> v = o.vec;
         vec = v;
         setWidth(v[0].size()*scale);
         setHeight(v.size()*scale);
     }
+
     void draw() const {
         for (int i=0; i<(getWidth()/scale); ++i) {
             for (int j=0; j<(getHeight()/scale); ++j) {
-                Rect tmp;
-                tmp.setSize(scale, scale);
-                tmp.setColor(vec[j][i]);
-                tmp.setCenter(((getLeftX() + (scale/2)) + (i*scale)),
-                               ((getTopY() + (scale/2)) + (j*scale)));
-                if (tmp.getRed() != 149.0/255.0) {
-                //if (tmp.getOpacity() != 0) {
+                if (vec[j][i] != nullopt) {
+                    Rect tmp;
+                    tmp.setSize(scale, scale);
+                    tmp.setColor(*vec[j][i]);
+                    tmp.setCenter(((getLeftX() + (scale/2)) + (i*scale)),
+                                  ((getTopY() + (scale/2)) + (j*scale)));
                     tmp.draw();
                 }
             }
@@ -70,12 +80,14 @@ Sprite initSprite(std::string filename) {
     inFile >> w;
     inFile >> h;
     for (int i=0; i<w; ++i) {
-        std::vector<color> tempVec = {};
+        std::vector<optional<color>> tempVec = {};
         for (int j=0; j<h; ++j) {
             inFile >> r;
             inFile >> g;
             inFile >> b;
-            tempVec.push_back(color(r/255.0, g/255.0, b/255.0));
+            // if (a == 0);
+            if (r == 149) tempVec.push_back(nullopt);
+            else tempVec.push_back(make_optional(color(r/255.0, g/255.0, b/255.0)));
         }
         tmp.vec.push_back(tempVec);
     }
