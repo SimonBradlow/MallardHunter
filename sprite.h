7a3c6d5d98ae09b63c445753755d13424f193e4e
Simon Bradlow <simon@bradlow.com>
2022-12-06 01:20:59 -0500

    sprite.h class refactored into one file for later repurposing

--- sprite.h
+++ sprite.h
@@ -11,11 +11,34 @@ private:
     double scale;
 public:
     std::vector<std::vector<color>> vec;
-    Sprite();
-    Sprite(double s);
-    void setScale(double s);
-    void setVec(std::vector<std::vector<color>> &v);
-    void draw() const override;
+    Sprite() {
+        scale = 1;
+    }
+    Sprite(double s) {
+        if (s > 0) scale = s;
+        else scale = 1;
+    }
+    void setScale(double s){
+        scale = s;
+    }
+    void setVec(std::vector<std::vector<color>> &v) {
+        vec = v;
+        setWidth(v[0].size()*scale);
+        setHeight(v.size()*scale);
+    }
+    void draw() const {
+        for (int i=0; i<(getWidth()/scale); ++i) {
+            for (int j=0; j<(getHeight()/scale); ++j) {
+                Rect temp;
+                temp.setSize(scale, scale);
+                temp.setColor(vec[j][i]);
+                temp.setCenter(getLeftX()+(i*scale)+(scale/2), getTopY()+(j*scale)+(scale/2));
+                if (temp.getOpacity() != 0) {
+                    temp.draw();
+                }
+            }
+        }
+    }
 };
 
 #endif //DUCKHUNT_SPRITE_H
