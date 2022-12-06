c1180a3a92e33840e67da2acf23673ef7bbbf053
Simon Bradlow <simon@bradlow.com>
2022-12-06 13:48:40 -0500

    spriteConverter.py implemented

--- sprite.h
+++ sprite.h
@@ -4,7 +4,16 @@
 #define DUCKHUNT_SPRITE_H
 
 #include "rect.h"
+#include <fstream>
 #include <vector>
+
+#ifdef _WIN32
+// If your Windows machine runs Python in CLI with "python" instead of "py", update this line.
+const std::string python = "py";
+#else
+// If your Mac/Linux machine runs Python in CLI with "python3" instead of "python", update this line.
+const std::string python = "python3";
+#endif
 
 class Sprite : public Rect {
 private:
@@ -26,19 +35,52 @@ public:
         setWidth(v[0].size()*scale);
         setHeight(v.size()*scale);
     }
+    void setVec(Sprite &o) {
+        std::vector<std::vector<color>> v = o.vec;
+        vec = v;
+        setWidth(v[0].size()*scale);
+        setHeight(v.size()*scale);
+    }
     void draw() const {
         for (int i=0; i<(getWidth()/scale); ++i) {
             for (int j=0; j<(getHeight()/scale); ++j) {
-                Rect temp;
-                temp.setSize(scale, scale);
-                temp.setColor(vec[j][i]);
-                temp.setCenter(getLeftX()+(i*scale)+(scale/2), getTopY()+(j*scale)+(scale/2));
-                if (temp.getOpacity() != 0) {
-                    temp.draw();
+                Rect tmp;
+                tmp.setSize(scale, scale);
+                tmp.setColor(vec[j][i]);
+                tmp.setCenter(((getLeftX() + (scale/2)) + (i*scale)),
+                               ((getTopY() + (scale/2)) + (j*scale)));
+                if (tmp.getRed() != 149.0/255.0`) {
+                //if (tmp.getOpacity() != 0) {
+                    tmp.draw();
                 }
             }
         }
     }
 };
+
+Sprite initSprite(std::string filename) {
+    Sprite tmp;
+
+    std::string command;
+    command = python + " ../spriteConverter.py " + filename;
+    system(command.c_str());
+
+    std::ifstream inFile("spriteData.txt");
+    int w, h, r, g, b, a;
+    inFile >> w;
+    inFile >> h;
+    for (int i=0; i<w; ++i) {
+        std::vector<color> tempVec = {};
+        for (int j=0; j<h; ++j) {
+            inFile >> r;
+            inFile >> g;
+            inFile >> b;
+            tempVec.push_back(color(r/255.0, g/255.0, b/255.0));
+        }
+        tmp.vec.push_back(tempVec);
+    }
+    inFile.close();
+    return tmp;
+}
 
 #endif //DUCKHUNT_SPRITE_H
